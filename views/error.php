a0a6caccff3c754f414c5a89fc918d15c88d09c9
Rony Lee <rongyil33@gmail.com>
2023-07-12 10:58:59 +0800

    Fix error page

--- views/error.php
+++ views/error.php
@@ -2,9 +2,8 @@
 <?php include "partials/navbar.php" ?>
 
 <div class="row">
-    <div class="col-6 offset-3">
+    <div class="col-6 offset-3 mt-5 pt-5">
         <div class="alert alert-danger" role="alert">
-            <h4 class="alert-header"><%=err.message%></h4>
             <p>
                 Aww yeah, you successfully managed to read this super important error
                 text message. Good job on your side! Please do better in the future to
@@ -15,9 +14,6 @@
                 yourself!
             </p>
             <h5 class="my-2">ERROR!!</h5>
-            <% if (process.env.NODE_ENV !== "production") { %>
-            <p><%=err.stack%></p>
-            <% } %>
         </div>
     </div>
 </div>
