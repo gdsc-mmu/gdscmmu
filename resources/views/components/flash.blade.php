a76a8f495c7db11e08b392d9c134e3f2bef701de
Rony Lee <rongyil33@gmail.com>
2024-03-21 06:18:46 +0800

    Customize flash message

--- resources/views/components/flash.blade.php
+++ resources/views/components/flash.blade.php
@@ -1,6 +1,6 @@
 @if(session()->has('message'))
 <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show">
-    <p class="position-absolute top-0 start-50 translate-middle-x bg-white rounded z-3" style="">
+    <p class="position-absolute top-0 start-50 translate-middle-x bg-white rounded z-3 px-4 py-1 mt-2">
         {{session('message')}}
     </p>
 </div>
