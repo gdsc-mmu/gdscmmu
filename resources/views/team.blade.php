d855186dddbab6a2223cd3159fda7eac273954f7
pipo <73412648+sakib2005@users.noreply.github.com>
2024-03-22 05:58:40 +0800

    updated the team page (#2)

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -188,13 +188,13 @@
                 </div>
             </div>
             <div data-aos="fade-down" data-aos-duration="1500" class="col-lg-4 col-md-6 col-sm-6">
-                <div class="profile">
+                <div class="profile" id="sakib">
                     <span class="profile-span"></span>
                     <span class="profile-span"></span>
                     <span class="profile-span"></span>
                     <span class="profile-span"></span>
                     <div class="profile-text">
-                        <h2>Hi, I'm Yuen Khai</h2>
+                        <h2>Hi, I'm Sakib</h2>
                         <p>Core Member</p>
                     </div>
                 </div>
