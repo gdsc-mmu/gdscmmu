9e1a6c8ad094ad03df6e06ee05bdce99317a8aa7
fikrisharudin20 <1221302093@student.mmu.edu.my>
2024-04-06 02:55:17 +0800

    Customize show event page

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -1,23 +1,27 @@
 <x-layout>
     <div class="d-flex align-items-center justify-content-center min-vh-100 flex-column">
         <h1>Show page</h1>
-        <p>{{ $event->title }}</p>
-        <p>{{ $event->description }}</p>
-        <p>{{ $event->date }}</p>
-        <p>{{ $event->time }}</p>
-        <p>Belongs to user id : {{ $event->user_id }}</p>
 
-        @if (Auth::check() && Auth::user()->id == $event->user_id)
-            <div class="d-flex">
-                <a href="/events/{{$event->id}}/edit" class="btn btn-warning">
-                    Edit event
-                </a>
-                <form href="/events/{{$event->id}}" method="POST">
-                    @method('DELETE')
-                    @csrf
-                    <button class="btn btn-danger ms-2">Delete event</button>
-                </form>
+        <div class="card mb-4" style="max-width: 600px;">
+            <img src="{{ asset('images/' . $event->image) }}" class="card-img-top img-fluid rounded workshop-graphic" alt="Event poster">
+            <div class="card-body">
+    
+                <h2 class="card-title">{{ $event->title }}</h2>
+                <p class="card-text">Description: {{ $event->description }}</p>
+                <p class="card-text">Date Created: {{ $event->date }}, {{ $event->time }}</p>
+                <p class="card-text">Belongs to user id: {{ $event->user_id }}</p>
+
+                @if (Auth::check() && Auth::user()->id == $event->user_id)
+                    <div class="d-flex">
+                        <a href="/events/{{$event->id}}/edit" class="btn btn-warning btn-sm me-2">Edit event</a>
+                        <form href="/events/{{$event->id}}" method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <button class="btn btn-danger btn-sm">Delete event</button>
+                        </form>
+                    </div>
+                @endif
             </div>
-        @endif
+        </div>
     </div>
 </x-layout>
