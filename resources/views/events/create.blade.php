21e49e78110da905845a55e33296cdc6904b7958
NIK1221303 <124517975+NIK1221303@users.noreply.github.com>
2024-04-28 10:50:32 +0800

    Implement create event UI into Create Event Page

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,8 +1,17 @@
-<div class="container">
-  <h1>Add New Event</h1>
-  <section class="mt-3">
-    <form method="post" action="{{ route('events.store') }}" enctype="multipart/form-data">
-      @csrf
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add New Event</title>
+    <link href="{{asset("css/addEvent.css")}}" rel="stylesheet">
+</head>
+<body>
+  <div class="container">
+    <div class="add-event-form">
+      <h1>Add New Event</h1>
+      <form method="post" action="{{ route('events.store') }}" enctype="multipart/form-data">
+ @csrf
       @if ($errors->any())
         <div class="alert alert-danger">
           <ul>
@@ -12,22 +21,44 @@
           </ul>
         </div>
       @endif
-      <div class="card p-3 mb-3">
-        <label for="floatingInput">Event Name</label>
-        <input class="form-control" type="text" name="title">
-      </div>
-      <div class="card p-3 mb-3">
-        <label for="floatingTextArea">Description</label>
-        <textarea class="form-control" name="description" id="floatingTextarea" cols="30" rows="10"></textarea>
-      </div>
-      <div class="card p-3 mb-3">
-        <label for="formFile" class="form-label">Add Image</label>
-        <img src="" alt="" class="img-blog">
-        <input class="form-control" type="file" name="image">
-      </div>
-      <div class="card p-3 mb-3">
-        <button class="btn btn-secondary" type="submit">ADD EVENT</button>
-      </div>
-    </form>
-  </section>
-</div>
+      <div class="form-group">
+          <label for="title" class="label-right"></label>
+          <input type="text"  class="form-group" placeholder=" Title*" name="title" >
+        </div>
+        <!-- <div class="date-time-group">
+            <div class="add-was-form">
+            <div class="date">
+              <label for="date">Date *</label>
+              <input type="date" id="date" name="date" >
+            </div>
+        </div>
+            <div class="time">
+                <div class="add-was-form">
+              <label for="time">Time *</label>
+              <input type="time" id="time" name="time" >
+            </div>
+        </div>
+          </div> -->
+        <div class="form-group">
+            <div class="form-group">
+                <label for="text" class="label-right"></label>
+                <textarea id="text" name="description" columns="20" placeholder="Description"  ></textarea>
+              </div>
+          </div>
+
+
+        <div class="form-group">
+          <label for="image">Insert image </label>
+          <div class="file-input-wrapper">
+            <input type="file" id="image" name="image" required>
+            <span class="file-input-text"></span>
+          </div>
+        </div>
+        <div class="button">
+          <button type="submit">Post</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
