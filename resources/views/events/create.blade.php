a71613a64d2572280ee933dbd1f428b8e271f4d8
Rony Lee <rongyil33@gmail.com>
2024-05-03 12:39:11 +0800

    Add form control

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,7 +4,7 @@
         <div id="event-container">
             <div class="add-event-form">
                 <h1 id="new-title">Add New Event</h1>
-                <form method="post" action="{{ route('events.store') }}" enctype="multipart/form-data">
+                <form method="post" action="{{ route('events.store') }}" enctype="multipart/form-data" class="needs-validation" novalidate>
                     @csrf
                     @if ($errors->any())
                         <div class="alert alert-danger">
@@ -16,38 +16,22 @@
                         </div>
                     @endif
                     <div class="form-group">
-                        <label for="title" class="label-right"></label>
-                        <input type="text"  class="form-group" placeholder=" Title*" name="title" >
+                        <label for="title" class="form-label text-white">Title</label>
+                        <input type="text"  class="form-control" placeholder=" Title*" name="title" >
                     </div>
-                        <!-- <div class="date-time-group">
-                            <div class="add-was-form">
-                            <div class="date">
-                            <label for="date">Date *</label>
-                            <input type="date" id="date" name="date" >
-                            </div>
-                            </div>
-                            <div class="time">
-                            <div class="add-was-form">
-                            <label for="time">Time *</label>
-                            <input type="time" id="time" name="time" >
-                            </div>
-                            </div>
-                            </div> -->
                             <div class="form-group">
                                 <div class="form-group">
-                                    <label for="text" class="label-right"></label>
-                                    <textarea id="text" name="description" columns="20" placeholder="Description"  ></textarea>
+                                    <label for="text" class="form-label text-white">Description</label>
+                                    <textarea id="text" class="form-control" name="description" columns="20" placeholder="Description"  ></textarea>
                                 </div>
                             </div>
 
 
                             <div class="form-group">
-                                <label for="image">Insert image </label>
-                                <div class="file-input-wrapper">
-                                    <input type="file" id="image" name="image" required>
-                                    <span class="file-input-text"></span>
-                                </div>
+                                <label for="image" class="form-label text-white">Insert image </label>
+                                <input type="file" id="image" name="image" class="form-control">
                             </div>
+
                             <div class="button">
                                 <button type="submit">Post</button>
                             </div>
