600d0dfdef4f30df15d919eefda84611cfd2354e
Rony Lee <rongyil33@gmail.com>
2024-05-04 19:42:17 +0800

    Add gray background to login, inverse text color

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,14 +3,14 @@
     <div class="d-flex align-items-center justify-content-center min-vh-100 flex-column">
         <form action="/login" method="POST" class="needs-validation" novalidate>
             @csrf
-            <label for="name" class="form-label">Username:</label>
+            <label for="name" class="form-label text-white">Username:</label>
             <input type="text" class="form-control" name="name" value="{{old('name')}}" required>
-            <label for="password" class="form-label">Password:</label>
+            <label for="password" class="form-label text-white">Password:</label>
             <input type="password" class="form-control" name="password" required>
-            <input type="submit" class="btn border border-dark rounded mt-2" value="Login">
+            <input type="submit" class="btn border rounded mt-2 text-white" value="Login">
         </form>
 
-        <span class="mt-5">Forgot your<a href="/recovery" class="text-decoration-none ms-1">password?</a></span>
-        <span>Haven't got an Account?<a href="/signup" class="text-decoration-none ms-2">Sign Up</a></span>
+        <span class="mt-5 text-white">Forgot your<a href="/recovery" class="text-decoration-none ms-1 text-warning">password?</a></span>
+        <span class="text-white">Haven't got an Account?<a href="/signup" class="text-decoration-none ms-2 text-warning">Sign Up</a></span>
     </div>
 </x-layout>
