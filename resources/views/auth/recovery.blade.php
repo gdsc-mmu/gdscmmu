3fe4d799415913ae1a8cc4bc512209d0e5650c91
Rony Lee <rongyil33@gmail.com>
2024-05-04 19:45:26 +0800

    Add gray background to recovery, inverse text color

--- resources/views/auth/recovery.blade.php
+++ resources/views/auth/recovery.blade.php
@@ -1,12 +1,13 @@
 <x-layout>
+    <link rel="stylesheet" href="{{asset('css/login.css')}}">
     <div class="d-flex align-items-center justify-content-center min-vh-100">
         <form action="/recovery" method="POST">
             @csrf
             <div class="form-group mb-3">
-                <label for="email">Email :</label>
+                <label for="email" class="text-white">Email :</label>
                 <input type="email" name="email" id="email" class="form-control">
             </div>
-            <button type="submit" class="btn btn-primary">Reset password</button>
+            <button type="submit" class="btn border rounded text-white">Reset password</button>
         </form>
     </div>
 </x-layout>
