a418cc6e9a63df8cbf57148a0b5bcc9acc82e28c
Rony Lee <rongyil33@gmail.com>
2024-05-11 21:13:55 +0800

    Disable migration

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,6 +3,6 @@
 echo "Running composer install..."
 composer install --no-dev --working-dir=/var/www/html
 
-echo "Running migrations..."
-php artisan migrate --force
-php artisan storage:link
+#echo "Running migrations..."
+#php artisan migrate --force
+#php artisan storage:link
