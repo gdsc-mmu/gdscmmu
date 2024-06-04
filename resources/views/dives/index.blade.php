06e196c08a5b9728cc4225e9ba7858f9978e1cfa
Rony Lee <rongyil33@gmail.com>
2024-06-04 23:41:58 +0800

    Add padding at top of dives index

--- resources/views/dives/index.blade.php
+++ resources/views/dives/index.blade.php
@@ -1,20 +1,23 @@
 <x-layout>
-    <div class="min-vh-100 d-flex justify-content-center align-items-center flex-column">
-        <h1 class="text-white">Weekly Dives</h1>
-        @if($dives->count())
-            @foreach($dives as $dive)
-                <h2 class="text-white">Dive {{$dive->id}}: {{$dive->title}}</h2>
-                <p class="text-white">{{$dive->date}}</p>
-                <p class="text-white">{{$dive->time}}</p>
-                <p class="text-white">{{$dive->description}}</p>
-            @endforeach
-        @else
-            <h2 class="text-white">Dive 1: Lorem velit eligendi?</h2>
-            <p class="text-white">Ipsum assumenda rem voluptatibus omnis nisi. Vero enim nisi vitae!</p>
-            <h2 class="text-white">Dive 2: Ipsum a voluptates.</h2>
-            <p class="text-white">Consectetur voluptas libero id explicabo numquam. Quisquam repellendus deserunt veniam.</p>
-            <h2 class="text-white">Dive 3: Lorem dolorum vitae</h2>
-            <p class="text-white">Lorem praesentium autem dolores saepe voluptate. Perspiciatis doloremque fuga aut?</p>
-        @endif
+    <link rel="stylesheet" href="{{asset('css/dives.css')}}">
+    <div id="dives" class="pt-5 z-0">
+        <div class="min-vh-100 d-flex justify-content-center align-items-center flex-column pt-5 z-1 container">
+            <h1 class="text-white">Weekly Dives</h1>
+            @if($dives->count())
+                @foreach($dives as $dive)
+                    <h3 class="text-white">Dive {{$dive->id}}: {{$dive->title}}</h2>
+                    <p class="text-white">{{$dive->date}}</p>
+                    <p class="text-white">{{$dive->time}}</p>
+                    <p class="text-white">{{$dive->description}}</p>
+                @endforeach
+            @else
+                <h3 class="text-white">Dive 1: Lorem velit eligendi?</h2>
+                <p class="text-white">Ipsum assumenda rem voluptatibus omnis nisi. Vero enim nisi vitae!</p>
+                <h3 class="text-white">Dive 2: Ipsum a voluptates.</h2>
+                <p class="text-white">Consectetur voluptas libero id explicabo numquam. Quisquam repellendus deserunt veniam.</p>
+                <h3 class="text-white">Dive 3: Lorem dolorum vitae</h2>
+                <p class="text-white">Lorem praesentium autem dolores saepe voluptate. Perspiciatis doloremque fuga aut?</p>
+            @endif
+        </div>
     </div>
 </x-layout>
