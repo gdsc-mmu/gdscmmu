83efd84d2da25a4a7447e388269d05196c294eeb
Rony Lee <rongyil33@gmail.com>
2024-06-07 23:10:35 +0800

    Reformat file

--- resources/views/dives/index.blade.php
+++ resources/views/dives/index.blade.php
@@ -4,20 +4,24 @@
         <div class="min-vh-100 d-flex justify-content-center align-items-center flex-column pt-5 z-1 container">
             <h1 class="text-white">Weekly Dives</h1>
             @if($dives->count())
-                @foreach($dives as $dive)
-                    <h3 class="text-white">Dive {{$dive->id}}: {{$dive->title}}</h2>
+            @foreach($dives as $dive)
+            <a href="/dives/{{$dive->id}}">
+                <h3 class="text-white">Dive {{$dive->id}}: {{$dive->title}}</h2>
                     <p class="text-white">{{$dive->date}}</p>
                     <p class="text-white">{{$dive->time}}</p>
                     <p class="text-white">{{$dive->description}}</p>
-                @endforeach
+            </a>
+            @endforeach
             @else
-                <h3 class="text-white">Dive 1: Lorem velit eligendi?</h2>
+            <h3 class="text-white">Dive 1: Lorem velit eligendi?</h2>
                 <p class="text-white">Ipsum assumenda rem voluptatibus omnis nisi. Vero enim nisi vitae!</p>
                 <h3 class="text-white">Dive 2: Ipsum a voluptates.</h2>
-                <p class="text-white">Consectetur voluptas libero id explicabo numquam. Quisquam repellendus deserunt veniam.</p>
-                <h3 class="text-white">Dive 3: Lorem dolorum vitae</h2>
-                <p class="text-white">Lorem praesentium autem dolores saepe voluptate. Perspiciatis doloremque fuga aut?</p>
-            @endif
+                    <p class="text-white">Consectetur voluptas libero id explicabo numquam. Quisquam repellendus
+                        deserunt veniam.</p>
+                    <h3 class="text-white">Dive 3: Lorem dolorum vitae</h2>
+                        <p class="text-white">Lorem praesentium autem dolores saepe voluptate. Perspiciatis doloremque
+                            fuga aut?</p>
+                        @endif
         </div>
     </div>
 </x-layout>
