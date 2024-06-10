2ad7af9c82dbaa358b2c01454641e91694d6f800
Rony Lee <rongyil33@gmail.com>
2024-06-10 23:57:34 +0800

    Make event show page smaller

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -1,23 +1,23 @@
 <x-layout>
     <link rel="stylesheet" href="{{asset('css/events.css')}}">
-    <div class="row p-5">
+    <div class="row p-4">
         <div class="col-md-8 offset-md-2 mt-5">
             <!-- Event Banner Section -->
             <div data-aos="fade-down" data-aos-duration="1500" class="mt-2 mb-4 text-center banner-section">
                 <img src="{{$event->image ? asset('images/' . $event->image) : 'https://images.unsplash.com/photo-1520004434532-668416a08753?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}}"
                     class="img-fluid mx-auto d-block rounded-start rounded-end shadow"
-                    style="max-width: auto; height: auto;" alt="Event banner">
+                    style="max-width: auto; height: 300px;" alt="Event banner">
             </div>
 
             <!-- Event Title Section -->
             <div data-aos="fade-down" data-aos-duration="1500" class="text-center mb-4">
-                <h1 style="color: white;">{{ $event->title }}</h1>
+                <h1 class="text-white fs-1">{{ $event->title }}</h1>
             </div>
 
             <!-- About the Event Section -->
             <section data-aos="fade-down" data-aos-duration="1500" class="bg-light p-4 rounded mb-4 shadow">
                 <div class="text-center">
-                    <h2 class="mb-4">About the Event</h2>
+                    <h2 class="mb-4 fs-2">About the Event</h2>
                     <hr style="border-top: 2px solid black; margin: 20px 0;">
                     <p class="card-text" style="font-size: 1.1rem;">{!!nl2br($event->description)!!}</p>
                 </div>
